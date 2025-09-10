8f87f9f9440dbe994a8c56a2b1da242ed5fd3e2a
amandagolinski <golinski.amanda@escola.pr.gov.br>
2025-09-10 12:30:26 +0000

    Fizemos 9 cartões no js

---
+++ perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'O que é JavaScript(JS)?',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'Para que serve a tag article?',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'O que é a tag footer?',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'Qual a diferença entre a tag title que usamos na tag head e a tag h1 que usamos na tag body?',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'Qual a diferença entre as tags head e header?',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'Pra que serve o :root utilizado no CSS',
+    resposta
+)
+criaCartao(
+    'Programação',
+    'Qual a diferença entre margin e padding?',
+    resposta
+)
